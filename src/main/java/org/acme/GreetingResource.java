4e4ed199e1e3884c1e644ac6a5cc428081e2d4b2
kubealex <al.rossi87@gmail.com>
2022-11-03 10:56:38 +0100

    histogram

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -10,9 +10,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+
 import io.micrometer.core.annotation.Timed;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Metrics;
 import io.micrometer.core.instrument.Tags;
+import io.micrometer.core.instrument.Timer;
 
 @Path("/hello")
 public class GreetingResource {
@@ -21,11 +24,17 @@ public class GreetingResource {
     MeterRegistry registry;
 
     @GET
-    @Timed("test")
+    //@Timed(histogram = true)
     @Path("/exec")
     public String execute() throws InterruptedException {
-        Thread.sleep(1);
-        registry.timer("testings-timer", Tags.of("name", "timer")).record(Duration.ofMillis(1000));;
+        Random duration = new Random();
+        Integer millis = duration.nextInt(4000);
+        Timer timer = Timer.builder("my-awesome-timer").tag("name", "sample").publishPercentiles(0.95).publishPercentileHistogram(true).register(registry);
+        timer.record(Duration.ofMillis(millis));
+        // timer.record(Duration.ofMillis(1109));
+        // timer.record(Duration.ofMillis(8909));
+        // timer.record(Duration.ofMillis(909));
+        //registry.timer("testings-timer", Tags.of("name", "timer")).record(Duration.ofMillis(1000));
         return "OK";
         // registry.timer("correlation_timer", Tags.of("name", "timer")).record(Duration.ofMillis(timeInMillis));
         // return new String("{\"exec\": \"success\", \"time\": " + timeInMillis + "}");
