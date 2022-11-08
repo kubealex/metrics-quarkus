c77d05f61511562a3c1036dab90e2ce99d52d5b5
kubealex <al.rossi87@gmail.com>
2022-11-08 12:33:24 +0100

    update

--- src/main/java/org/acme/MetricsResource.java
+++ src/main/java/org/acme/MetricsResource.java
@@ -1,24 +1,17 @@
 package org.acme;
 
-import java.time.Duration;
-import java.util.Random;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-
-import io.micrometer.core.annotation.Timed;
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Metrics;
-import io.micrometer.core.instrument.Tags;
-import io.micrometer.core.instrument.Timer;
+import org.jboss.logging.Logger;
 
 @Path("/")
 public class MetricsResource {
+    private static final Logger LOG = Logger.getLogger(MetricsResource.class);
 
     @Inject
     TestFunction testFunction;
@@ -27,6 +20,7 @@ public class MetricsResource {
     @Path("/run")
     public String execute() throws InterruptedException {
         testFunction.loadAction();
+        LOG.info("Traced");
         return "OK";
     }
 
